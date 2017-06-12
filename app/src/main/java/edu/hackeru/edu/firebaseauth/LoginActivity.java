8a80c910e477cd5155fc29c62576a3fcdc1a5b7b
hackerudroids <hackerudroids@gmail.com>
2017-06-12 21:30:38 +0300

    Basic login and registration

--- app/src/main/java/edu/hackeru/edu/firebaseauth/LoginActivity.java
+++ app/src/main/java/edu/hackeru/edu/firebaseauth/LoginActivity.java
@@ -1,10 +1,18 @@
 package edu.hackeru.edu.firebaseauth;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
 
 import com.beardedhen.androidbootstrap.BootstrapButton;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -12,6 +20,9 @@ import butterknife.OnClick;
 
 
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
     @BindView(R.id.etEmail)
     EditText etEmail;
     @BindView(R.id.etPassword)
@@ -26,6 +37,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+        mAuth = FirebaseAuth.getInstance();
     }
 
     private String getEmail() {
@@ -57,13 +69,72 @@ public class LoginActivity extends AppCompatActivity {
 
     @OnClick(R.id.btnLogin)
     public void onBtnLoginClicked() {
+        //client side validation
+        if (!isEmailValid() | !isPasswordValid()) return;
+
+        showProgress(true);
+        String email = getEmail();
+        String password = getPassword();
+        //INTERNET PERMISSION
+        mAuth.signInWithEmailAndPassword(email, password).
+                addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        showProgress(false);
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
+                    }
+                }).
+                addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        showProgress(false);
+                        Snackbar.make(btnLogin, e.getMessage(), Snackbar.LENGTH_INDEFINITE).show();
+                    }
+                });
+    }
+
+    ProgressDialog dialog;
+    private void showProgress(boolean show){
+        if (dialog == null){
+            dialog = new ProgressDialog(this/*Context*/);
+            //title
+            dialog.setTitle("Connecting to server");
+            //message
+            dialog.setMessage("Please Wait");
+            //cancelable
+            dialog.setCancelable(false);
+        }
+        if (show)
+            dialog.show();
+        else
+            dialog.dismiss();
     }
 
     @OnClick(R.id.btnRegister)
     public void onBtnRegisterClicked() {
         //client side validation
         if (!isEmailValid() | !isPasswordValid()) return;
+
+        showProgress(true);
         String email = getEmail();
         String password = getPassword();
+        //INTERNET PERMISSION
+        mAuth.createUserWithEmailAndPassword(email, password).
+                addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        showProgress(false);
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
+                    }
+                }).
+                addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        showProgress(false);
+                        Snackbar.make(btnLogin, e.getMessage(), Snackbar.LENGTH_INDEFINITE).show();
+                    }
+                });
     }
 }
